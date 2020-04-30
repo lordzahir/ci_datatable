5ff72fe318e29c53fbc44f4a560ee524525a2326
lordzahir <zfplayss@gmail.com>
2020-04-30 10:29:59 +0700

    write some hint

---
+++ application/controllers/Barang.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Barang extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_barang');
+	}
+	
+	public function index(){
+		$x['data']=$this->m_barang->show_barang();
+		$this->load->view('v_barang',$x);
+	}
+}
---
+++ application/models/M_barang.php
@@ -0,0 +1,9 @@
+<?php
+class M_barang extends CI_Model{
+	
+	function show_barang(){
+		$hasil=$this->db->query("SELECT * FROM tbl_barang");
+		return $hasil;
+	}
+	
+}
